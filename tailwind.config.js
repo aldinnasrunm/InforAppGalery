8e5dc168a2f0628f9cca919ed8f554ad590e2ecf
aldinnasrun <adnzain40@gmail.com>
2024-07-17 11:53:39 +0700

    add chatbotmu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html","./dist/*.{html,js}"],
+  content: ["./dist/index.html","./dist/chatbotmu.html","./dist/*.{html,js}"],
   theme: {
     extend: {},
   },
